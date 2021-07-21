133398f07d4d8eff988a69ad9bb015c506ac5559
Kshitij148 <raokshitij14@gmail.com>
2021-07-21 15:16:54 +0530

    Registration form  error messages updated

--- src/components/register.js
+++ src/components/register.js
@@ -47,17 +47,45 @@ const Register = () => {
                      }
                     
                 }
-               if (username==null || username=="" || password==null || password=="" || email==null || email=="" || number==null || number==""){  
+               if (username==null || username=="" || password==null || password=="" || email==null || email=="" || number==null || number==""){
+                        document.getElementById("us-rerror").style.display= "none";
+                        document.getElementById("ps-rerror").style.display= "none";
+                        document.getElementById("em-rerror").style.display= "none";
+                        document.getElementById("pn-rerror").style.display= "none";
+                        document.getElementById("gen-rerror").innerHTML = "All Fields Must be filled";
+                        document.getElementById("gen-rerror").style.display= "block";
                      
                    }
                else if(password.length<8){  
+                          document.getElementById("gen-rerror").style.display= "none";
+                        document.getElementById("us-rerror").style.display= "none";
+                        document.getElementById("em-rerror").style.display= "none";
+                        document.getElementById("pn-rerror").style.display= "none";
+                         document.getElementById("ps-rerror").innerHTML = "Password must be atleast of Length 6";
+                         document.getElementById("ps-rerror").style.display= "block";
+                     
+                    
                     
                     }
                 else if(!validator.isEmail(email))
-                  {
+                {
+                           document.getElementById("gen-rerror").style.display= "none";
+                         document.getElementById("us-rerror").style.display= "none";
+                        document.getElementById("ps-rerror").style.display= "none";
+                        document.getElementById("pn-rerror").style.display= "none";
+                         document.getElementById("em-rerror").innerHTML = "Invalid Email Address";
+                         document.getElementById("em-rerror").style.display= "block";
+                     
                          
                     }
                else if(flag==0){
+                           document.getElementById("gen-rerror").style.display= "none";
+                        document.getElementById("ps-rerror").style.display= "none";
+                        document.getElementById("em-rerror").style.display= "none";
+                        document.getElementById("pn-rerror").style.display= "none";
+                        document.getElementById("us-rerror").innerHTML = "Username Already Exists";
+                        document.getElementById("us-rerror").style.display= "block";
+                     
                         
                         setData1("");
 
@@ -105,16 +133,34 @@ const Register = () => {
                 <span><i className="fa fa-user"></i></span>
             </div>
             <div className="form-group">
+                 <div>
+                    <center>
+                    <p id= "gen-rerror" style={{display:"none",color:"red"}}>  </p>
+                    </center>
+                   <br />
+                </div>
                 <input type="text" className="form-control item" id="username" placeholder="Username" value={username} onChange={e => setData1(e.target.value)}/>
+                <div>
+                    <p id= "us-rerror" style={{display:"none",color:"red"}}>  </p>
+                </div>
             </div>
             <div className="form-group">
                 <input type="password" className="form-control item" id="password" placeholder="Password" value={password} onChange={e => setData2(e.target.value)} />
-            </div>
+                <div>
+                    <p id= "ps-rerror" style={{display:"none",color:"red"}}>  </p>
+                </div>
+             </div>
             <div className="form-group">
                 <input type="text" className="form-control item" id="email" placeholder="Email" value={email} onChange={e => setData3(e.target.value)} />
+                <div>
+                    <p id= "em-rerror" style={{display:"none",color:"red"}}>  </p>
+                </div>
             </div>
             <div className="form-group">
                 <input type="text" className="form-control item" id="phone-number" placeholder="Phone Number" value={number} onChange={e => setData4(e.target.value)} />
+                <div>
+                    <p id= "pn-rerror" style={{display:"none",color:"red"}}>  </p>
+                </div>
             </div>
            
             <div className="form-group">
